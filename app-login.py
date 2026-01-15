bf6d75442857f3d807499b81fbca5b4a1d01f2c4
SigmaNotBetaStudent <jose.grosso@itiscuneo.eu>
2026-01-15 10:33:08 +0100

    flask-login con control.html e login.html

--- app-login.py
+++ app-login.py
@@ -1,106 +1,132 @@
+# from flask import Flask, render_template, redirect, url_for, request, flash
+# from flask_login import (
+#     LoginManager, UserMixin,
+#     login_user, login_required, 
+#     logout_user, current_user
+# )
+# import sqlite3
+# import Alphabot
+
+# app = Flask(__name__)
+# app.secret_key = 'chiave segreta'
+
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+# login_manager.login_view = 'login'
+
+# robot = Alphabot.AlphaBot()
+# class User(UserMixin):
+#     def __init__(self, id):
+#         self.id = id
+#     USERS = {
+
+#     }
+
+# def load_user(user, id):
+#     if user_id in USERS:
+#         return User(user_id)
+#     return None
+
+# # da fare def load_user():
+# @app.route("/", methods=["GET", "POST"])
+# @app.route("/login", methods=["GET", "POST"])
+# def login():
+#     if request.method =="POST":
+#         username = request.form.get("username")
+#         password = request.form.get("password")
+#         #query = "SELECT * FROM Utenze WHERE User = ? AND Psw = ?"
+#         if username in USERS and USERS[username]["password" ] == password:
+#             login_user(User(username))
+#             return redirect(url_for("control"))
+
+#     return render_template("login.html")
+# @app.route("/control")
+# @login_required
+# def control():
+#     return render_template("control.html")
+
+# @app.route("/logout")
+# @login_required
+# def logout():
+#     logout_user()
+#     return redirect(url_for("login"))
 from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_login import (
     LoginManager, UserMixin,
     login_user, login_required, 
     logout_user, current_user
 )
-import sqlite3
-from werkzeug.security import check_password_hash
-import Alphabot
+import AlphaBot
 
 app = Flask(__name__)
-app.secret_key = 'chiave segreta'
+app.secret_key = 'chiave_segreta_molto_piu_sicura_cambiala!'  # Cambia questa chiave!
 
+# Inizializzazione Flask-Login
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-robot = Alphabot.AlphaBot()
+robot = AlphaBot.AlphaBot()
+robot.stop()
 
-# Classe User per Flask-Login
 class User(UserMixin):
-    def __init__(self, id, username):
+    def __init__(self, id):
         self.id = id
-        self.username = username
 
-# Funzione per connessione al database
-def get_db_connection():
-    conn = sqlite3.connect('utenze.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+USERS = {
+    "admin": {"password": "admin123"},
+    "utente1": {"password": "password1"}
+}
 
-# Carica l'utente dal database
 @login_manager.user_loader
 def load_user(user_id):
-    conn = get_db_connection()
-    user_data = conn.execute('SELECT * FROM Utenze WHERE Id = ?', (user_id,)).fetchone()
-    conn.close()
-    if user_data:
-        return User(user_data['Id'], user_data['User'])
+    if user_id in USERS:
+        return User(user_id)
     return None
 
-# Verifica credenziali
-def verify_user(username, password):
-    conn = get_db_connection()
-    user_data = conn.execute('SELECT * FROM Utenze WHERE User = ?', (username,)).fetchone()
-    conn.close()
-    
-    if user_data:
-        # Se le password sono in chiaro nel DB (non consigliato in produzione)
-        if user_data['Psw'] == password:
-            return User(user_data['Id'], user_data['User'])
-        # Se le password sono hashate
-        # if check_password_hash(user_data['Psw'], password):
-        #     return User(user_data['Id'], user_data['User'])
-    return None
-
-# Route di login
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         
-        user = verify_user(username, password)
-        if user:
-            login_user(user)
-            next_page = request.args.get('next')
-            return redirect(next_page or url_for('index'))
+        if username in USERS and USERS[username]["password"] == password:
+            login_user(User(username))
+            flash(f"Benvenuto {username}!", "success")
+            return redirect(url_for("control"))
         else:
-            flash('Username o password non corretti', 'error')
+            flash("Credenziali non valide!", "error")
     
-    return render_template('login.html')
+    return render_template("login.html")
+
+# Route di controllo robot
+@app.route("/control", methods=["GET", "POST"])
+@login_required
+def control():
+    if request.method == "POST":
+        if "Avanti" in request.form:
+            robot.forward()
+        elif "Indietro" in request.form:
+            robot.backward()
+        elif "Destra" in request.form:
+            robot.right()
+        elif "Sinistra" in request.form:
+            robot.left()
+        elif "Stop" in request.form:
+            robot.stop()
+        
+        return render_template("control.html", username=current_user.id)
+    
+    return render_template("control.html", username=current_user.id)
 
 # Route di logout
-@app.route('/logout')
+@app.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('login'))
+    flash("Logout effettuato con successo!", "success")
+    return redirect(url_for("login"))
 
-# Route principale con joystick
-@app.route('/', methods=['GET', 'POST'])
-@login_required
-def index():
-    if request.method == 'POST':
-        # Gestione comandi dal joystick
-        if 'Avanti' in request.form:
-            robot.forward()
-        elif 'Indietro' in request.form:
-            robot.backward()
-        elif 'Sinistra' in request.form:
-            robot.left()
-        elif 'Destra' in request.form:
-            robot.right()
-        elif 'Stop' in request.form:
-            robot.stop()
-        
-        return '', 204  # Risposta vuota per chiamate AJAX
-    
-    return render_template('index.html', username=current_user.username)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0")
